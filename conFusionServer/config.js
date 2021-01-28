c8ea2916fa27f794383ba2e3448abd894794ac32
Mukhrit <54068781+Mukhrit@users.noreply.github.com>
2021-01-28 21:35:43 +0530

    outh 2.0

--- conFusionServer/config.js
+++ conFusionServer/config.js
@@ -1,4 +1,8 @@
 module.exports = {
   secretKey: '12345-67890-09876-54321',
   mongoUrl: 'mongodb://localhost:27017/conFusion',
+  facebook: {
+    clientId: '267716281361991',
+    clientSecret: 'b92981dc561a8d870fb1e674a00dde00',
+  },
 };
